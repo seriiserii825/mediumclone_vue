b48e890deddd4bfd32256db100ccbd33669f15b8
serii <seriiburduja@gmail.com>
2021-01-25 16:31:42 +0200

    prettier

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+}
+</script>
 
 <style lang="scss">
 #app {
