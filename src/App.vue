ac2231600a343720c607abea2226d96e81de489a
serii <seriiburduja@gmail.com>
2021-01-25 20:49:05 +0200

    register page

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,25 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/register">Register</router-link>
     </div>
     <router-view />
   </div>
 </template>
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  height: 100vh;
   color: #2c3e50;
+  background-color: lightgray;
 }
 
 #nav {
---
+++ src/views/Register.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          Validations error
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="email"
+                class="form-control form-control-lg"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-primary pull-xs-right">Sign up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'McvRegister',
+  methods: {
+    onSubmit() {},
+  },
+}
+</script>
+<style lang="scss">
+.form-group {
+  margin-bottom: 0.3rem;
+}
+</style>
